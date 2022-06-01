dec2bd310c987ef4ebd3f5dd90d5d908bba82852
Krishna-Yangale <krishna.yangale@radiant.co>
2022-06-01 11:22:54 +0530

    modified addition program

--- 02addition/add.go
+++ 02addition/add.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("addition is :", 10+9)
+
+	var number1 int = 56
+	var number2 int = 69
+	var number3 float64 = 69.5
+
+	fmt.Println("The addition is :", number1+number2+int(number3))
+	fmt.Println("the Addition is :", float64(number1)+float64(number2)+number3)
 }
