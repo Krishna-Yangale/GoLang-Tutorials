fbf67e9efa89e40f60e0b6e80a56d94367610bcc
Krishna-Yangale <krishna.yangale@radiant.co>
2022-06-02 13:25:22 +0530

    email changed

--- 12Structs/main.go
+++ 12Structs/main.go
@@ -6,7 +6,7 @@ func main() {
 
 	//different ways to print data using struct
 	fmt.Println("Welcome to strcut in golang")
-	krishna := User{"Krishna", "krishna.yangale@radiant.co", true, 24}
+	krishna := User{"Krishna", "krishna.yangale@radiantcloud.co", true, 24}
 	fmt.Println(krishna)
 
 	fmt.Printf("The details are :%+v\n", krishna)
